e23597644504aae4a527090585b05d8c99717adb
MnPutrav2 <mochnurdinv2@gmail.com>
2026-07-25 09:17:19 +0700

    update build path

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ const __dirname = path.dirname(__filename);
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "final/web",
+    outDir: "build/web",
     assetsDir: "assets",
     emptyOutDir: true
   },
